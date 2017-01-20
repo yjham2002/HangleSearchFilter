63ae5f7434c8ba0e6c954f7c23c611a60b646b9e
yjham2002 <yjham2002@gmail.com>
2017-01-20 17:58:58 +0900

    README modified

--- README.md
+++ README.md
@@ -1,3 +1,35 @@
 # 한글검색필터
 
-- 개발자가 많이 피곤하여 리드미는 나중에 작성됩니당 ㅠ
+- 한글 및 한글의 초성 기반의 단어 검색을 수행하는 필터
+
+- 사용 예시
+
+```java
+ public static void main(String[] args){
+        List<InputKeywordDBean> words = new ArrayList<InputKeywordDBean>();
+
+        /**
+         * Arbitrary Data
+         * */
+        words.add(new InputKeywordDBean("1단어(숫자로시작하는글자)"));
+        words.add(new InputKeywordDBean("단어(★가나다)"));
+        words.add(new InputKeywordDBean("a도쿄"));
+        words.add(new InputKeywordDBean("a돔b쿄"));
+        words.add(new InputKeywordDBean("동영상"));
+        words.add(new InputKeywordDBean("다양성"));
+        words.add(new InputKeywordDBean("대나무"));
+        words.add(new InputKeywordDBean("라면"));
+        words.add(new InputKeywordDBean("고속도로"));
+        words.add(new InputKeywordDBean("라디오"));
+        words.add(new InputKeywordDBean("다리미"));
+        words.add(new InputKeywordDBean("*동영상+시청"));
+
+        /**
+         * Constructing
+         * */
+        HangleSearchFilter hFilter = new HangleSearchFilter(words);
+        String searchChar = "aㄷ";
+
+        for(String s : hFilter.search(10, searchChar)) System.out.println(s);
+    }
+```
--- src/AppMain.java
+++ src/AppMain.java
@@ -3,30 +3,29 @@ import java.util.List;
 
 public class AppMain {
     public static void main(String[] args){
-
         List<InputKeywordDBean> words = new ArrayList<InputKeywordDBean>();
 
         /**
          * Arbitrary Data
          * */
-        words.add(new InputKeywordDBean("1다이마루(★가나다)"));
-        words.add(new InputKeywordDBean("담이마루(★가나다)"));
+        words.add(new InputKeywordDBean("1단어(숫자로시작하는글자)"));
+        words.add(new InputKeywordDBean("단어(★가나다)"));
         words.add(new InputKeywordDBean("a도쿄"));
         words.add(new InputKeywordDBean("a돔b쿄"));
         words.add(new InputKeywordDBean("동영상"));
-        words.add(new InputKeywordDBean("도레미파솔"));
+        words.add(new InputKeywordDBean("다양성"));
         words.add(new InputKeywordDBean("대나무"));
         words.add(new InputKeywordDBean("라면"));
-        words.add(new InputKeywordDBean("도로로로롱"));
+        words.add(new InputKeywordDBean("고속도로"));
         words.add(new InputKeywordDBean("라디오"));
         words.add(new InputKeywordDBean("다리미"));
-        words.add(new InputKeywordDBean("*다이+마루가나다라"));
+        words.add(new InputKeywordDBean("*동영상+시청"));
 
         /**
          * Constructing
          * */
         HangleSearchFilter hFilter = new HangleSearchFilter(words);
-        String searchChar = "담이마루(★";
+        String searchChar = "aㄷ";
 
         for(String s : hFilter.search(10, searchChar)) System.out.println(s);
     }
